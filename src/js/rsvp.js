9897f10a562864c4efa73aee63b50bf9a1994ad1
yvonnerogell <yvonnerogell@users.noreply.github.com>
2019-06-07 16:12:16 -0700

    Added formspree to rsvp page

---
+++ src/js/rsvp.js
@@ -0,0 +1,12 @@
+document.getElementById("testing").addEventListener("click", myFunction); 
+
+
+function myFunction() {
+  var x = document.getElementById("myDiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
