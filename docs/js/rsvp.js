be4cb8a8a856c67b173815c85b33c6fd6e6c6a79
Hannah Hamavid <hamavid@Hannahs-computer.local>
2019-06-14 14:57:40 -0700

    adding some attempts to predict weird behavior on the rsvp form and changing css for all pages to work in more browsers - firefox, safari, chrome

--- docs/js/rsvp.js
+++ docs/js/rsvp.js
@@ -15,13 +15,19 @@ $(document).ready(function(){
     "X97ER": "Test Guest, Another Tester",
   }
 
+  // reactivate search button if text box is clicked
+  $('.search-container').click(function(e){
+    $('.search-container button, #indeed').removeClass('inactivebutton');
+  });
+
   // functions to find names by code (key) and report it in the result div when the key button is clicked
   $('.search-container button').click(function(e){
     // change all css back to how it would be on refresh // NOTE: IS THERE MORE? THINK THIS THROUGH
     $('.broken').removeClass('highlight');
-    $('#result, #rsvp-form div').html('');
-    $('#rsvp-form, #rsvp-form section').hide();
-    $('#rvsp-form input').val('');
+    $('#confirm-buttons').css('display','none');
+    $('#result').html('');
+    $('section, button[type=submit]').remove();
+    $('#rsvp-form').hide();
     e.preventDefault();
     // deal with input text
     var input = $('#namesinput').val().toUpperCase();
@@ -56,41 +62,46 @@ $(document).ready(function(){
 
 
 /* ///////////// Functions to toggle sections depending on num guests and responses for each guest //////////////*/
-// If 'nope try again' button is clicked, clear text field, result field, and highlight the link to alt-rsvp page in red
-    $('#notso').click(function(){
-      $('#namesinput').val('');
-      $('#result, #rsvp-form div').html('');
-      $('#rsvp-form, #rsvp-form section').hide();
-      $('#rvsp-form input').val('');
-      $('.broken, .broken a').addClass('highlight');
-    });
+  // If 'nope try again' button is clicked, clear text field, result field, and highlight the link to alt-rsvp page in red
+  $('#notso').click(function(){
+    $('#namesinput').val('');
+    $('#confirm-buttons').css('display','none');
+    $('#result').html('');
+    $('#rsvp-form').hide();
+    $('section, button[type=submit]').remove();
+    $('.broken, .broken a').addClass('highlight');
+  });
 
-    // If 'yes that's me/us!' button is clicked, show appropriate number of will/won't attends and populate name field
-    $('#indeed').click(function(){
-      // deal with input text
-      var input = $('#namesinput').val().toUpperCase(); //uppercase it in case people were inconsistent
-      // if there is no guestlist entry for this code, display error message
-      if (names[input] === undefined) {
-        var result = "Hmmm that doesn't look right. Check that your code is 5 characters long and contains only letters and numbers.";
-        $('#result').html(result);
-        return;
-      // if guestlist entry is found, continue
-      } else {
-        var respondingfor=names[input];
-        var numppl = respondingfor.split(', ').length;
-        // show rsvp form
-        $('#rsvp-form').show();
-        // create attendance options and comment/diet fields for each possible guest and customize to the guest
-        for (var i=0;i<numppl;i++) {
-          buildform(i);
-          var guestname = respondingfor.split(', ')[i];
-          $('input[name="name'+i+'"').val(guestname);
-          $('#diet'+i+' div').html('Optional: Tell us something!<br>If '+guestname.split(' ')[0]+' <em>will</em> be attending, please let us know if they have any food allergies or dietary restrictions.');
-          $('#diet'+i).show();
-        }
-        $('#rsvp-form form').append('<button type="submit">Submit</button>');
+  // If 'yes that's me/us!' button is clicked, show appropriate number of will/won't attends and populate name field
+  $('#indeed').click(function(){
+    // make this button and search button not do anything and empty out anything currently in the rsvp form
+    $('section, button[type=submit]').remove();
+    $('.search-container button, #indeed').addClass('inactivebutton');
+    // deal with input text
+    var input = $('#namesinput').val().toUpperCase(); //uppercase it in case people were inconsistent
+    // if there is no guestlist entry for this code, display error message
+    if (names[input] === undefined) {
+      var result = "Hmmm that doesn't look right. Check that your code is 5 characters long and contains only letters and numbers.";
+      $('#result').html(result);
+      return;
+    // if guestlist entry is found, continue
+    } else {
+      var respondingfor=names[input];
+      var numppl = respondingfor.split(', ').length;
+      // show rsvp form
+      $('#rsvp-form').show();
+      // create attendance options and comment/diet fields for each possible guest and customize to the guest
+      for (var i=0;i<numppl;i++) {
+        buildform(i);
+        var guestname = respondingfor.split(', ')[i];
+        $('input[name="name'+i+'"]').val(guestname);
+        //$('#diet'+i+' div').html('Optional: Tell us something!<br>If '+guestname.split(' ')[0]+' <em>will</em> be attending, please let us know if they have any food allergies or dietary restrictions.');
+        $('#diet'+i+' div').html('Optional: Please let us know about any food allergies or dietary restrictions for ' + guestname.split(' ')[0] + ', or anything else you want to tell us!');
+        $('#diet'+i).show();
       }
-    })
+      $('#rsvp-form form').append('<button type="submit">Submit</button>');
+    }
+  })
 
   // Function to build an RSVP section per guest
   function buildform(i){
@@ -104,7 +115,7 @@ $(document).ready(function(){
     var comment_section = $('<section></section').attr('id', 'diet'+i);
     $('#rsvp-form form').append(comment_section);
     comment_section.append('<div></div>');
-    comment_section.append('<textarea name="diet'+i+'" placeholder="Enter allergies/dietary restrictions, or anything else you want to tell us!" rows="4" cols="35"></textarea>');
+    comment_section.append('<textarea name="diet'+i+'" placeholder="Enter allergies/dietary restrictions, or anything else you want to tell us!" rows="4"></textarea>');
   }
 
 
